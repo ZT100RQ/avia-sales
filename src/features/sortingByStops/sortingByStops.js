fba4723a2d5a3a38f94b485ddd1de2a7f55a9e78
Azat Khabirov <zt100rq@gmail.com>
2024-10-05 19:21:06 +0500

    aside filter + some fix

---
+++ src/features/sortingByStops/sortingByStops.js
@@ -0,0 +1,32 @@
+export default function sortingByStops(listOfTickets, stops) {
+  if (stops?.all) return listOfTickets;
+  let sortedTickets = [];
+  const test = new Map([
+    ['one', 1],
+    ['two', 2],
+    ['three', 3],
+    ['zero', 0],
+  ]);
+
+  const listOfStops = Object.keys(stops);
+  // all, zero, one = true , two = true, three
+  listOfStops.forEach((element) => {
+    if (stops[element]) {
+      if (!sortedTickets.length) {
+        const result = listOfTickets.filter((ticket) => {
+          return ticket.segments[0].stops.length > ticket.segments[1].stops.length
+            ? ticket.segments[1].stops.length == test.get(element)
+            : ticket.segments[0].stops.length == test.get(element);
+        });
+        sortedTickets = [...result];
+      }
+      const result = listOfTickets.filter((ticket) => {
+        return ticket.segments[0].stops.length > ticket.segments[1].stops.length
+          ? ticket.segments[1].stops.length == test.get(element)
+          : ticket.segments[0].stops.length == test.get(element);
+      });
+      sortedTickets = [...sortedTickets, ...result];
+    }
+  });
+  return sortedTickets;
+}
